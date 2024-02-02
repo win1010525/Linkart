e726c223ab6f629ad2b7aaf7d04401289bd1cbc8
melontini <104443436+melontini@users.noreply.github.com>
2024-02-02 23:52:54 +0700

    Clean up mixin and fix another linking issue

--- src/main/java/com/github/vini2003/linkart/mixin/PlayerEntityMixin.java
+++ src/main/java/com/github/vini2003/linkart/mixin/PlayerEntityMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -29,41 +30,30 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     void onInteract(Entity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         if (entity instanceof AbstractMinecartEntity minecart) {
             if (!getWorld().isClient()) {
-                ServerWorld world = ((ServerWorld) entity.getWorld());
                 PlayerEntity player = (PlayerEntity) (Object) this;
                 ItemStack stack = player.getStackInHand(hand);
 
                 if (stack.isIn(Linkart.LINKERS)) {
                     if (Linkart.UNLINKING_CARTS.containsKey(player)) {
                         AbstractMinecartEntity unlinking = Linkart.UNLINKING_CARTS.get(player);
-                        if (unlinking == null) {
-                            world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                            cir.setReturnValue(ActionResult.FAIL);
-                        } else if (unlinking == minecart) {
-                            world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                            cir.setReturnValue(ActionResult.FAIL);
+                        if (unlinking == null || unlinking == minecart) {
+                            fail(cir, minecart);
                         } else {
                             if (unlinking.linkart$getFollower() == minecart) {
                                 CartUtils.unlink(minecart);
                                 cir.setReturnValue(ActionResult.SUCCESS);
                             } else {
-                                world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                                cir.setReturnValue(ActionResult.FAIL);
+                                fail(cir, minecart);
                             }
                         }
                         Linkart.UNLINKING_CARTS.remove(player);
                     } else if (Linkart.LINKING_CARTS.containsKey(player)) {
                         AbstractMinecartEntity linkingTo = Linkart.LINKING_CARTS.get(player);
 
-                        if (linkingTo == null) {
-                            world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                            cir.setReturnValue(ActionResult.FAIL);
-                        } else if (linkingTo == minecart) {
-                            world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                            cir.setReturnValue(ActionResult.FAIL);
-                        } else if (Math.abs(minecart.distanceTo(linkingTo) - 1) > Linkart.CONFIG.pathfindingDistance) {
-                            world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                            cir.setReturnValue(ActionResult.FAIL);
+                        if (linkingTo == null || linkingTo == minecart ||
+                                minecart.linkart$getFollower() == linkingTo ||
+                                Math.abs(minecart.distanceTo(linkingTo) - 1) > Linkart.CONFIG.pathfindingDistance) {
+                            fail(cir, minecart);
                         } else {
                             if (!player.isCreative()) stack.decrement(1);
                             CartUtils.link(minecart, linkingTo, stack);
@@ -72,15 +62,25 @@ public abstract class PlayerEntityMixin extends LivingEntity {
                         Linkart.LINKING_CARTS.remove(player);
                     } else if (minecart.linkart$getFollower() != null) {
                         Linkart.UNLINKING_CARTS.put(player, minecart);
-                        world.spawnParticles(ParticleTypes.HAPPY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                        cir.setReturnValue(ActionResult.SUCCESS);
+                        success(cir, minecart);
                     } else {
                         Linkart.LINKING_CARTS.put(player, minecart);
-                        world.spawnParticles(ParticleTypes.HAPPY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
-                        cir.setReturnValue(ActionResult.SUCCESS);
+                        success(cir, minecart);
                     }
                 }
             }
         }
+    }
+
+    @Unique
+    private static void fail(CallbackInfoReturnable<ActionResult> cir, AbstractMinecartEntity minecart) {
+        ((ServerWorld) minecart.getWorld()).spawnParticles(ParticleTypes.ANGRY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
+        cir.setReturnValue(ActionResult.FAIL);
+    }
+
+    @Unique
+    private static void success(CallbackInfoReturnable<ActionResult> cir, AbstractMinecartEntity minecart) {
+        ((ServerWorld) minecart.getWorld()).spawnParticles(ParticleTypes.HAPPY_VILLAGER, minecart.getX(), minecart.getY() + 0.2, minecart.getZ(), 10, 0.5, 0.5, 0.5, 0.5);
+        cir.setReturnValue(ActionResult.SUCCESS);
     }
 }
